b4cfbf15eec64e9a648727bf557a33886698ee34
KatKatKateryna <kateryna@speckle.systems>
2025-07-24 20:48:55 +0100

    create locales for EN and PT

---
+++ i18n/pygeoapi_config.pro
@@ -0,0 +1,7 @@
+FORMS = ../pygeoapi_config_dialog_base.ui
+
+SOURCES = ../pygeoapi_config.py \
+../pygeoapi_config_dialog.py
+
+TRANSLATIONS = pygeoapi_config_en_EN.ts \
+tablemanager_pt_PT.ts 
---
+++ i18n/tablemanager_pt_PT.ts
@@ -0,0 +1,390 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>PygeoapiConfig</name>
+    <message>
+        <location filename="../pygeoapi_config.py" line="177"/>
+        <source>&amp;pygeoapi configurator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config.py" line="164"/>
+        <source>Configure pygeoapi</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PygeoapiConfigDialogBase</name>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="14"/>
+        <source>pygeoapi configurator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="36"/>
+        <source>Brought to you with ❤️ by ByteRoad</source>
+        <translation>Trazido até si com ❤️ pela ByteRoad</translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="50"/>
+        <source>Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="56"/>
+        <source>bind</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="62"/>
+        <source>000.0.0.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="69"/>
+        <source>host</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="76"/>
+        <source>port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="900"/>
+        <source>url</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="115"/>
+        <source>map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="124"/>
+        <source>attribution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="146"/>
+        <source>gzip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="153"/>
+        <source>pretty print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="160"/>
+        <source>admin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="167"/>
+        <source>cors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="185"/>
+        <source>encoding</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="193"/>
+        <source>utf-8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="204"/>
+        <source>limits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="212"/>
+        <source>default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="233"/>
+        <source>On Exceed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="241"/>
+        <source>throttle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="246"/>
+        <source>error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="258"/>
+        <source>maximum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="289"/>
+        <source>mimetype</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="297"/>
+        <source>application/json; charset=UTF-8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="309"/>
+        <source>templates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="317"/>
+        <source>path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="495"/>
+        <source>📂</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="348"/>
+        <source>static</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="392"/>
+        <source>languages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="411"/>
+        <source>en-US</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="416"/>
+        <source>en-GB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="421"/>
+        <source>fr-CA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="426"/>
+        <source>fr-FR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="431"/>
+        <source>pt-PT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="442"/>
+        <source>logging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="450"/>
+        <source>level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="458"/>
+        <source>DEBUG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="463"/>
+        <source>INFO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="468"/>
+        <source>ERROR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="473"/>
+        <source>WARNING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="485"/>
+        <source>logfile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="518"/>
+        <source>logformat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="533"/>
+        <source>dateformat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="565"/>
+        <source>Metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="573"/>
+        <source>identification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="998"/>
+        <source>title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="1008"/>
+        <source>description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="605"/>
+        <source>keywords</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="620"/>
+        <source>Enter keyword to add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="627"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="644"/>
+        <source>Delete Selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="657"/>
+        <source>keywords type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="669"/>
+        <source>terms of service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="699"/>
+        <source>license</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="780"/>
+        <source>name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="736"/>
+        <source>provider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="773"/>
+        <source>contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="792"/>
+        <source>position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="804"/>
+        <source>address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="816"/>
+        <source>city</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="828"/>
+        <source>stateorprovince</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="840"/>
+        <source>postalcode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="852"/>
+        <source>country</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="864"/>
+        <source>phone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="876"/>
+        <source>fax</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="888"/>
+        <source>email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="912"/>
+        <source>hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="924"/>
+        <source>instructions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="936"/>
+        <source>role</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="957"/>
+        <source>Resources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="963"/>
+        <source>select collection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="975"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="982"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pygeoapi_config_dialog_base.ui" line="992"/>
+        <source>collection details</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
