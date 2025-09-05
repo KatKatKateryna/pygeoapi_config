710392563a9163c96abee4250e6eb08824f32831
KatKatKateryna <89912278+KatKatKateryna@users.noreply.github.com>
2025-09-05 12:37:21 +0100

    Kate/unit tests (#6)
    * adjust datetime format
    
    * run tests on PR, show failed tests
    
    * add pytests for file open-save
    
    * typo
    
    * separate the workflow
    
    * absolute import
    
    * stop ignoring yml on commit
    
    * import top level repo
    
    * run pytests from root
    
    * run pytests from the workspace
    
    * update imports in test folder
    
    * init file for tests
    
    * mode importlib
    
    * import qgis as optional
    
    * avoiding qgis imports if possible
    
    * fix imports
    
    * optional typing for qgis imports
    
    * same
    
    * make pyqt5 offscreen mode
    
    * run tests in a virtual display
    
    * Install PyQt5 dependencies manually
    
    * switch to qbot instead of QApplication
    
    * only init dialog
    
    * remove typos
    
    * YAML for windows
    
    * runt test
    
    * ignore Qgs imports
    
    * run a bit more
    
    * print URL
    
    * print from start
    
    * show print statements
    
    * print statements flush
    
    * more prints
    
    * check file location
    
    * more prints
    
    * wrap QgsMessageLog to try/except
    
    * print new file path for checking
    
    * write wrong YAML
    
    * Revert "write wrong YAML"
    
    This reverts commit dd7a6e41db9f7ab20cdcba6d3901fb9f8458bb59.
    
    * fix test
    
    * remove datatime validation (for testing)
    
    * Revert "remove datatime validation (for testing)"
    
    This reverts commit 627f488083e410352371aad9d361c63d92f16142.

---
+++ tests/test_yaml_save.py
@@ -0,0 +1,39 @@
+import os
+import pytest
+import subprocess
+
+from ..pygeoapi_config_dialog import PygeoapiConfigDialog
+
+
+@pytest.mark.parametrize("sample_yaml", ["docker.config.yml"])
+def test_json_schema(qtbot, sample_yaml: str):
+    """Validate YAML against schema.json after loading and saving."""
+
+    # Create the dialog widget and let qtbot manage it
+    dialog = PygeoapiConfigDialog()
+    qtbot.addWidget(dialog)
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # directory of current file
+
+    # Load YAML
+    abs_yaml_path = os.path.join(base_dir, sample_yaml)
+    dialog.open_file(abs_yaml_path)  # now dialog.config_data has the data stored
+
+    # Save YAML
+    new_yaml_name = f"saved_{sample_yaml}"
+    abs_new_yaml_path = os.path.join(base_dir, new_yaml_name)
+    dialog.save_to_file(abs_new_yaml_path)
+
+    result = subprocess.run(
+        [
+            "check-jsonschema",
+            "--schemafile",
+            "https://raw.githubusercontent.com/geopython/pygeoapi/refs/heads/master/pygeoapi/resources/schemas/config/pygeoapi-config-0.x.yml",
+            abs_new_yaml_path,
+        ],
+        capture_output=True,
+        text=True,
+    )
+    print(f"_______File saved as '{abs_new_yaml_path}'", flush=True)
+
+    assert result.returncode == 0, f"Validation failed:\n{result.stderr}"
