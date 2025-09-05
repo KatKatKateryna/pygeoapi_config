054081fd583f995e3def1c0a28105fe914c9bc58
KatKatKateryna <kateryna@speckle.systems>
2025-09-05 14:01:24 +0100

    shorten tests

--- tests/test_yaml_save.py
+++ tests/test_yaml_save.py
@@ -48,8 +48,8 @@ def test_json_schema_on_open_save(qtbot, base_dir, sample_yaml: str):
     "sample_yaml",
     ["docker.config.yml", "pygeoapi-test-config-ogr.yml", "cite.config.yml"],
 )
-def test_json_schema_on_open_save_with_ui_validation(qtbot, base_dir, sample_yaml: str):
-    """Validate YAML against schema.json after loading and saving."""
+def test_open_file_validate_ui_data(qtbot, base_dir, sample_yaml: str):
+    """Run UI data validation from opened file (done in the plugin before saving to the new file)."""
 
     # Create the dialog widget and let qtbot manage it
     dialog = PygeoapiConfigDialog()
@@ -65,22 +65,7 @@ def test_json_schema_on_open_save_with_ui_validation(qtbot, base_dir, sample_yam
         # 2 sample files are expected to fail the UI validation (incomplete "Hello" Resource data)
         # only fail the test if a legit file ("docker.config.yml") did not pass the validation
         if sample_yaml == "docker.config.yml":
-            assert False, f"UI data is not valid: {invalid_props}"
+            assert False, f"'{sample_yaml}' file UI data is not valid: {invalid_props}"
 
-    # Save YAML
-    new_yaml_name = f"saved_{sample_yaml}"
-    abs_new_yaml_path = os.path.join(base_dir, new_yaml_name)
-    dialog.save_to_file(abs_new_yaml_path)
-
-    result = subprocess.run(
-        [
-            "check-jsonschema",
-            "--schemafile",
-            "https://raw.githubusercontent.com/geopython/pygeoapi/refs/heads/master/pygeoapi/resources/schemas/config/pygeoapi-config-0.x.yml",
-            abs_new_yaml_path,
-        ],
-        capture_output=True,
-        text=True,
-    )
-    print(f"_______File saved as '{abs_new_yaml_path}'", flush=True)
-    assert result.returncode == 0, f"Validation failed:\n{result.stderr}"
+    print(f"_________Data validated for '{sample_yaml}'", flush=True)
+    assert True
